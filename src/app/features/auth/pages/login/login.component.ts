0cd3cb848f7c83c3bf86b69fe24bd9f60f99e0d6
Flavio <flaviocesar3433@gmail.com>
2025-11-21 03:50:14 -0500

    Implementación de estilo en versión Angular, tanto para Login y Registro de la página

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,43 +1,44 @@
-import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule, RouterLink, Router, Route } from '@angular/router';
-import { AuthService } from '../../../../core/services/auth.service';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgIf } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "../../../../core/services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  standalone:true,
-  imports: [ReactiveFormsModule, NgIf],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  selector: "app-login",
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf, RouterLink],
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  error = "";
 
-onSubmit() {
-  if(this.loginForm.invalid) return;
-  this.authService.login(this.loginForm.value).subscribe({
-    next:(res)=>{
-      localStorage.setItem('token', res.token);
-      localStorage.setItem('rol',res.rol);
-      localStorage.setItem('correo',res.correo);
-      this.router.navigate(['/home-post'])
-    },
-    error: (err)=>{
-      this.error=err.error || 'Credenciales incorrectas';
-    }
-  })
-}
-error: string="Credenciales incorrectas";
-loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      correo: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(5)]],
+    });
+  }
 
-constructor(private fb: FormBuilder , private authService : AuthService, private router : Router){
-  this.loginForm=this.fb.group(
-    {
-      correo: ['',[Validators.required,Validators.email]],
-      password : ['', [Validators.required,Validators.minLength(5)]],
-    },
-  )
-}
+  onSubmit() {
+    if (this.loginForm.invalid) return;
 
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (res) => {
+        localStorage.setItem("token", res.token);
+        localStorage.setItem("rol", res.rol);
+        localStorage.setItem("correo", res.correo);
+        this.router.navigate(["/home-post"]);
+      },
+      error: (err) => {
+        this.error = err.error || "Credenciales incorrectas";
+      },
+    });
+  }
 }
